9c829261abd071bee712b7b75bd54249a32928a7
Emerson Linhares <adaide.antonio@gmail.com>
2023-06-22 21:34:53 -0300

    inserção exercicio 6

---
+++ Bruno/exec6.js
@@ -0,0 +1,14 @@
+// Dado o seguinte objeto:
+const carro = {
+    marca: 'Ford',
+    modelo: 'Mustang',
+    ano: 2021,
+    motor: {
+      cilindrada: 5000,
+      combustivel: 'gasolina'
+    }
+  };
+  
+  // Utilize destructuring para extrair as propriedades 'marca', 'modelo', 'combustivel' e 'potencia' do objeto carro.
+  // Atribua um valor padrão de 'Desconhecida' para a propriedade 'potencia' caso ela não exista no objeto.
+  
